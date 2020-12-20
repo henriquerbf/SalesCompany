fa3f7a080356b09a7066744fee45d4c4acc506e4
henriquerbf <henriquerbf@hotmail.com>
2020-12-19 23:59:49 -0300

    Context

---
+++ SalesCompany/Data/SalesCompanyContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesCompany.Models;
+
+namespace SalesCompany.Data
+{
+    public class SalesCompanyContext : DbContext
+    {
+        public SalesCompanyContext (DbContextOptions<SalesCompanyContext> options): base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+    }
+}
